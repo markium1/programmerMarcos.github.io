298277479e403b3dde0f0b8adea0733480e19e53
Marcos Soares <38232965+programmerMarcos@users.noreply.github.com>
2020-03-29 21:05:33 -0300

    Update requisicao.js

--- requisicao.js
+++ requisicao.js
@@ -1,3 +1,4 @@
+
 function fazerRequisicao(){
 
     var url = 'https://covid19.mathdro.id/api?fbclid=IwAR3O5qKjXCERD2pkA-Y6mSorXBrL1lvu8JgyUvN0UPyfUYjDSRQ5R-pGizs';
@@ -7,10 +8,60 @@ function fazerRequisicao(){
 
     xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
     dados = JSON.parse(xhttp.responseText);
-    console.log(dados.confirmed.value);
     
-    document.getElementById("qt_casos").innerText = dados.confirmed.value;
-    document.getElementById("qt_recuperados").innerText = dados.recovered.value;
-    document.getElementById("qt_mortes").innerText = dados.deaths.value;
-    document.getElementById("resposta").innerHTML = xhttp.responseText;
+    //document.getElementById("txt-card-casos").innerText = 'NUMERO DE CASOS EM TODO O MUNDO'
+    document.getElementById("qt_casos").innerText = 'CASOS CONFIRMADOS: ' + dados.confirmed.value;
+    document.getElementById("qt_recuperados").innerText = 'RECUPERADOS: ' + dados.recovered.value;
+    var letalidade = (dados.deaths.value/dados.confirmed.value)*100
+    document.getElementById("qt_mortes").innerText = 'LETALIDADE: ' + letalidade.toFixed(2) + '%';
+    covidBrasil()
+}
+
+function covidBrasil(){
+
+    var url = 'https://covid19.mathdro.id/api/countries/BR';
+
+    var xhttp = new XMLHttpRequest()
+    xhttp.open("GET", url, false);
+
+    xhttp.send();
+
+    dados= JSON.parse(xhttp.responseText);
+
+    document.getElementById("casos_br").innerText = 'Casos BR:' + dados.confirmed.value;
+    document.getElementById("recuperados_br").innerText = 'Recuperados BR: ' + dados.recovered.value;
+    document.getElementById("mortes_br").innerText = 'Mortes BR: ' + dados.deaths.value;
+    
+    console.log(dados);
+
+}
+
+function pesquisa_pais(){
+    var select = document.getElementById('seletor-pais');
+	var text = select.options[select.selectedIndex].value;
+    
+    var url = 'https://covid19.mathdro.id/api/countries/' + text;
+    console.log(url);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, false);
+
+    xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
+    dados = JSON.parse(xhttp.responseText);
+    if (dados.error){
+
+        document.getElementById("casos_pesquisa").innerText = 'Não temos registro na nossa API';
+        document.getElementById("recuperados_pesquisa").innerText = '';
+        document.getElementById("mortes_pesquisa").innerText = '';
+
+    }
+    else{
+
+        document.getElementById("casos_pesquisa").innerText = 'Casos: ' + dados.confirmed.value;
+        document.getElementById("recuperados_pesquisa").innerText = 'Recuperados: ' + dados.recovered.value;
+        document.getElementById("mortes_pesquisa").innerText = 'Mortes: ' + dados.deaths.value;
+
+    }
+    
+    
 }
