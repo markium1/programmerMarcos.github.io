c72824d7de101bd27575cda669cf936c6f0e0f52
Marcos Soares <38232965+programmerMarcos@users.noreply.github.com>
2020-10-26 12:32:04 -0300

    adicionando o arquivo main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+function validar() {
+    var nome = form1.nome.value;
+    var email = form1.email.value;
+    
+    if (nome == "") {
+        alert('Preencha o campo com seu nome');
+        form1.nome.focus();
+        return false;
+    }
+    
+    if (email == "") {
+        alert('Preencha o campo com um email valido');
+        form1.email.focus();
+        return false;
+    }
+
+    alert("Para aprender mais, entre no maior site de xadrez do mundo e faça seu cadastro!")
+    window.open("https://www.google.com")
+    
+}
